7c4fdc09d45c4e45334268fc9689a7816fb64efd
jimbo <jimbo@91932.com>
2023-08-25 15:36:02 +0900

    Controller作成

---
+++ app/Http/libraryController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Psy\Context;
+
+class libraryController extends Controller{
+    public function index(){
+        //
+        echo "<h1>図書管理システム</h1>";
+    }
+}

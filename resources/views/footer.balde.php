9e7d544c1efab888e1e9eb2d5f8420a63768469a
jimbo <jimbo@91932.com>
2023-08-25 15:25:27 +0900

    ビューファイル作成

---
+++ resources/views/footer.balde.php
@@ -0,0 +1,11 @@
+@section('footer')
+<footer class="footer">
+    <div class="container-fluid">
+        <nav class="pull-left"></nav>
+        <div class="copyright pull-right">
+            &copy; <script>document.write(new Date().getFullYear())</script>
+        </div>
+    </div>
+</footer>
+@endsection
+

f44201620d026ef61ba645bc1445b9a5cd67b592
jimbo <jimbo@91932.com>
2023-08-28 11:53:21 +0900

    課題５分の修正と開設後の反映

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,40 @@
-@extends('layouts.app')
-@yield('sidebar')
-@extends('main')
 
-@section('content')
+@extends('main')
+@include("footer")
+
+@section('sidebar')
+<div class="sidebar" data-background-color="white" data-active-color="danger">
+    <div class="sidebar-wrapper">
+        <div class="logo">
+            <a href="/library/index" class="simple-text">
+                XX  Library
+            </a>
+        </div>
+        <ul class="nav">
+            <li class="link">
+                <a href="/library/index">
+                    <i class="ti-book"></i>
+                    <p>Book List</p>
+                </a>
+            </li>
+            <li class="link active" id="link-book-list">
+                <a href="/library/history">
+                    <i class="ti-user"></i>
+                    <p>Borrow History</p>
+                </a>
+            </li>
+            <li class="link">
+                <a href="/logout">
+                    <i class="ti-back-left"></i>
+                    <p>Logout</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+@endsection
+
+@section('contents')
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
@@ -48,4 +80,3 @@
 </div>
 @endsection
 
-@include('footer')
